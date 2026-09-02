deb58262728e1e39b77418ffdf151b533c1546c7
dhanashree7-ctrl <dhanashree7-ctrl@users.noreply.github.com>
2026-09-02 11:07:04 +0530

    chore: remove unwanted test and dump files

--- backend/test_fcm.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/fcm_helper.php';
-$token = 'exwE3ZxmlK4is8aL9wDHt7:APA91bHM8wQhvrjDO7sZWcMiao0_sLCvOHVCtrwRBy96GN21qw7dAnHJDxRdsMl8MMFUkwX5mSV2GyQoEme9SQPWNwqHf8p3WcsbYvUT0K4Op91b965KaLE';
-$res = send_fcm_notification($token, 'Test', 'Test message', '/');
-var_dump($res);
